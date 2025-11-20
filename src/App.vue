198350e56bf9468119c5e7b2579606cc93049df8
SixtenE <sixten.ekblad@gmail.com>
2025-11-20 13:54:35 +0100

    refactor: remove current route path display from App component

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,5 @@
 <template>
   <h1>Hello App!</h1>
-  <p><strong>Current route path:</strong> {{ $route.fullPath }}</p>
   <nav>
     <RouterLink to="/">Go to Home</RouterLink>
     <RouterLink to="/counter">Go to About</RouterLink>
