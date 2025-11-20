56ddfb19ce53045263d48a804a33506e2c2ed337
SixtenE <sixten.ekblad@gmail.com>
2025-11-20 14:04:22 +0100

    feat: update HTML structure, enhance App component layout, and add Tailwind CSS

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,12 @@
 <template>
-  <h1>Hello App!</h1>
-  <nav>
-    <RouterLink to="/">Go to Home</RouterLink>
-    <RouterLink to="/counter">Go to About</RouterLink>
-  </nav>
-  <main>
-    <RouterView />
-  </main>
+  <div>
+    <h1>Hello App!</h1>
+    <nav>
+      <RouterLink to="/">Go to Home</RouterLink>
+      <RouterLink to="/counter">Go to About</RouterLink>
+    </nav>
+    <main>
+      <RouterView />
+    </main>
+  </div>
 </template>
--- src/CounterView.vue
+++ src/CounterView.vue
@@ -4,5 +4,11 @@ const counterStore = useCounterStore()
 </script>
 
 <template>
-  <h1>{{ counterStore.count }}</h1>
+  <h1>
+    {{ counterStore.count }}
+  </h1>
+  <h2>
+    {{ counterStore.doubleCount }}
+  </h2>
+  <button @click="counterStore.increment">Increment</button>
 </template>
